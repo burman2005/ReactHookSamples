98948efec3c9a82856fbe5aae581a5b6c4674b4e
Praveen Kumar <burman2005@gmail.com>
2024-04-16 10:30:17 +0000

    Add an router and form in react app.

--- src/index.js
+++ src/index.js
@@ -4,17 +4,31 @@ import App from "./App";
 import Counter from "./useEffect";
 import Car from "./useState";
 import Component1 from "./useContext";
-import PrevStateRef from "../useRef";
+import PrevStateRef from "./useRef";
 import Todos from "./useReducer";
 import CallBackApp from "./useCallback";
 import MemoApp from "./useMemo";
 import { Home } from "./customHook";
+import GarageListComp from "./List";
+import SubmitForm from "./MyForm";
+import RouterApp from "./Router";
+import './my-sass.scss';
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hello Style!</h1>
+      <p>Add a little style!.</p>
+    </>
+  );
+}
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Home />
+    <RouterApp />
+    <Header/>
   </StrictMode>
 );
